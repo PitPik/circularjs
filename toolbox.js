687c3c7b05f5cb0ea8f2678d2ded9bfaeac18c8d
Peter Dematté <PitPik@users.noreply.github.com>
2017-07-19 14:30:52 +0200

    Update toolbox.js

--- toolbox.js
+++ toolbox.js
@@ -2,7 +2,7 @@
 	if (typeof exports === 'object') {
 		module.exports = factory(root);
 	} else if (typeof define === 'function' && define.amd) {
-		define('Toolbox', [], function () {
+		define('toolbox', [], function () {
 			return factory(root);
 		});
 	} else {
