77c783e3998a27477f9f819be04a300a85f3f343
Peter Dematté <PitPik@users.noreply.github.com>
2017-12-22 16:42:41 +0100

    Update search.js

--- heroes/modules/dashboard/js/search.js
+++ heroes/modules/dashboard/js/search.js
@@ -3,15 +3,14 @@ function(Circular, heroService) {
     'use strict';
 
     var circular = new Circular(),
-        debounce = null,
         searchModel = {};
 
     circular.component('heroes-search', {
         model: [searchModel],
         eventListeners: {
             search: function(e, element, item) {
-                clearTimeout(debounce);
-                debounce = setTimeout(function() {
+                clearTimeout(this.debounce);
+                this.debounce = setTimeout(function() {
                     search(element.value);
                 }, 300);
             }
