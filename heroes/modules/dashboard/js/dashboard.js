6783b36a3734f196c2fdd172b6aef6b0f1293e18
Peter Dematté <PitPik@users.noreply.github.com>
2017-12-22 14:26:27 +0100

    Update dashboard.js

--- heroes/modules/dashboard/js/dashboard.js
+++ heroes/modules/dashboard/js/dashboard.js
@@ -13,3 +13,12 @@ function(Circular, heroService) {
         });
     }
 });
+
+/**
+ * This file is part of a module, the HTML and it's resources. This
+ * module returns a initialisation function 'init'. The purpose of
+ * this component inside 'init' is only to render the first 4 items
+ * in the hero list that we get via 'heroService.getHeroes()'.
+ *
+ * The rest of the module is defined in the HTML file (index.html)
+ */
