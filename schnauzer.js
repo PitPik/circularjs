0832858f31c2cbe7e90c945fbd09f446f68a3f4b
peterd <peter@dematte.at>
2018-07-21 19:54:14 +0200

    updated schnauzer to 1.3.0x

--- schnauzer.js
+++ schnauzer.js
@@ -5,7 +5,7 @@
     function () { return factory(root); });
   else root.Schnauzer = factory(root);
 }(this, function SchnauzerFactory(root, undefined) { 'use strict';
-// Schnauzer 5.18 KB, 2.26 KB, Mustage 5.50 KB, 2.27 KB, Handlebars 74.20 KB, 21.86 KB
+// Schnauzer 5.30 KB, 2.31 KB, Mustage 5.50 KB, 2.27 KB, Handlebars 74.20 KB, 21.86 KB
 var Schnauzer = function(template, options) {
     this.version = '1.2.0';
     this.options = {
@@ -22,10 +22,12 @@ var Schnauzer = function(template, options) {
       },
       doEscape: true,
       helpers: {},
+      decorators: {},
       partials: {},
       recursion: 'self',
       characters: '$"<>%-=@',
-      splitter: '|##|'
+      splitter: '|##|',
+      tools: undefined,
     };
     init(this, options || {}, template);
   },
@@ -67,6 +69,12 @@ Schnauzer.prototype = {
   unregisterHelper: function(name) {
     delete this.options.helpers[name];
   },
+  registerDecorator: function(name, fn) {
+    this.options.decorators[name] = fn;
+  },
+  unregisterDecorator: function(name, fn) {
+    delete this.options.decorators[name];
+  },
   registerPartial: function(name, html) {
     return this.partials[name] = sizzleTemplate(this, html);
   },
@@ -82,11 +90,11 @@ return Schnauzer;
 
 function switchTags(_this, tags) {
   var _tags = tags[0] === '{{' ? ['{{2,3}', '}{2,3}'] : tags;
-  var chars = _this.options.characters;
+  var chars = _this.options.characters + '\\][';
 
   _this.variableRegExp = new RegExp('(' + _tags[0] + ')' +
-    '([>!&=])*([\\w\\'+ chars + '\\.]+)\\s*([\\w' + chars + '\\.\\s]*)' + _tags[1], 'g');
-  _this.sectionRegExp = new RegExp('(' + _tags[0] + ')([#^])([\\w' + chars + ']*)' +
+    '([>!&=])*\\s*([\\w\\'+ chars + '\\.]+)\\s*([\\w' + chars + '\\.\\s]*)' + _tags[1], 'g');
+  _this.sectionRegExp = new RegExp('(' + _tags[0] + ')([#^*]*)\\s*([\\w' + chars + ']*)' +
     '(?:\\s+([\\w$\\s|./' + chars + ']*))*(' + _tags[1] + ')((?:(?!\\1[#^])[\\S\\s])*?)' +
     '\\1\\/\\3\\5', 'g');
   _this.elseSplitter = new RegExp(_tags[0] + 'else' + _tags[1]);
@@ -104,6 +112,7 @@ function getSource(data, extra, newData, helpers) {
   var isNew = !data.__schnauzerData;
   var _extra = hasNewData && !isNew && data.extra || [];
   var _helpers = !isNew && data.helpers || [];
+
   return {
     extra: extra ? concat(extra, _extra) : _extra,
     path: isNew ? [data] : hasNewData ? concat(data.path, [newData]) : data.path,
@@ -139,6 +148,7 @@ function getVar(text) {
   var parts = text.split(/\s*=\s*/);
   var value = parts.length > 1 ? parts[1] : parts[0];
   var isString = value.charAt(0) === '"' || value.charAt(0) === "'";
+  var isInline = false;
   var depth = 0;
   var keys = [];
   var path = [];
@@ -154,12 +164,21 @@ function getVar(text) {
     }
     name = name.replace(/^(?:\.|this)\//, function() { strict = true; return ''; });
     keys = value.split(/[\.\/]/);
-    value = value.replace(/^\.\//, function() { strict = true; keys[0] = '.'; return ''; });
+
+    value = value.replace(/^(\.\/|this\.|this\/|\*)/, function(all, $1) {
+      if ($1 === '*') {
+        isInline = true;
+        return '';
+      }
+      strict = true; keys[0] = '.'; return '';
+    }).replace(/(?:^\[|\]$)/g, '');
   }
+
   return {
     name: parts.length > 1 ? parts[0] : value,
     value: value,
     isString: isString,
+    isInline: isInline,
     strict: strict,
     keys: keys,
     depth: depth,
@@ -172,22 +191,19 @@ function escapeHtml(string, _this) {
   });
 }
 
-function tools(_this, data, parts, body, altBody) {
-  return {
-    getData: function getData(key) {
-      key = parts.rawParts[key] || { value: key, keys: [key], depth: 0 };
-      return key.isString ? key.value : findData(data, key.value, key.keys, key.depth);
-    },
-    escapeHtml: function escape(string) { return escapeHtml(string, _this) },
-    getBody: function() { return body && body(data) || '' },
-    gatAltBody: function() { return altBody && altBody(data) || '' },
-    data: data.path[0]
-  }
-}
-
-function addToHelper(helpers, keys, name, value) {
-  if (keys) { helpers[keys[0]] = value;  helpers[keys[1]] = name; }
-  return helpers;
+function tools(_this, fn, name, params, data, parts, body, altBody, out) {
+  return _this.options.tools ?
+    _this.options.tools(_this, findData, getSource, fn, name, params, data, parts, body, altBody) :
+    fn.apply({
+      getData: function getData(key) {
+        key = parts.rawParts[key] || { value: key, keys: [key], depth: 0 };
+        return key.isString ? key.value : findData(data, key.value, key.keys, key.depth);
+      },
+      escapeHtml: function escape(string) { return escapeHtml(string, _this) },
+      getBody: function() { return body && body(data) || '' },
+      gatAltBody: function() { return altBody && altBody(data) || '' },
+      data: data.path[0]
+    }, parts.isInline ? [function() { return body || '' }] : params);
 }
 
 function splitVars(_this, vars, _data, unEscaped, char0) {
@@ -206,11 +222,22 @@ function splitVars(_this, vars, _data, unEscaped, char0) {
     parts: parts,
     rawParts: rawParts,
     partial: char0 === '>' && (_this.partials[_data.name] || _this.partials[options.recursion]),
+    isInline: _data.isInline,
     isUnescaped: !options.doEscape || char0 === '&' || unEscaped,
     depth: _data.depth,
     strict: _data.strict,
     keys: _data.keys,
   };
+}
+
+function createHelper(value, name, keys, len, n) {
+  var helpers = len ? { '@index': n, '@last': n === len - 1, '@first': n === 0 } : {};
+
+  helpers['@key'] = name;
+  helpers['.'] = helpers['this'] = value;
+
+  if (keys) { helpers[keys[0]] = value;  helpers[keys[1]] = name; }
+  return helpers;
 }
 
 function inline(_this, html, sections) {
@@ -226,6 +253,7 @@ function inline(_this, html, sections) {
     }
     if (char0 === '!' || char0 === '=') return '';
     vars = vars.split(/\s+/); // split variables
+    if (name === '*') { name = name + vars.shift(); }
     keys.push(splitVars(_this, vars, getVar(name), start === '{{{', char0));
     return splitter;
   }).split(splitter);
@@ -235,6 +263,11 @@ function inline(_this, html, sections) {
       out = out + html[n];
       part = keys[n];
       if (part === undefined) continue; // no other functions, just html
+      if (part.isInline) {
+        out = tools(_this, _this.options.decorators[part.name || part.vars[0]],
+          part.name, part.vars, data, part, out) || out;
+        out = isFunction(out) ? out() : out; // TODO: optimise
+      }
       if (part.section) { out += sections[part.section](data) || ''; continue; }
       if (part.partial) { // partial -> executor
         newData = {}; // create new scope (but keep functions in scope)
@@ -244,11 +277,14 @@ function inline(_this, html, sections) {
           newData[key] = _data.isString ? _data.value :
             findData(data, _data.value, _data.keys, _data.depth);
         }
-        _out = part.partial(getSource(newData));
+        newData = getSource(newData);
+        newData.helpers = [data.helpers[0]];
+        newData.extra = [data.extra[0]];
+        _out = part.partial(newData);
       } else {
         _out = findData(data, part.name, part.keys, part.depth);
         _fn = !part.strict && _this.options.helpers[part.name] || isFunction(_out) && _out;
-        _out = _fn ? _fn.apply(tools(_this, data, part), part.vars) :
+        _out = _fn ? tools(_this, _fn, part.name, part.vars, data, part) :
           _out && (part.isUnescaped ? _out : escapeHtml(_out, _this));
       }
       if (_out !== undefined) out = out + _out;
@@ -266,30 +302,36 @@ function section(_this, fn, name, vars, unEscaped, isNot) {
 
   return function fastLoop(data) {
     var _data = findData(data, name.name, name.keys, name.depth);
+    var helper = !name.strict && (_this.options.helpers[name.name] || isFunction(_data) && _data);
+    var helperValue = helper && tools(_this, helper, name.name, vars.vars, data, vars, fn[0], fn[1]);
     var _isArray = isArray(_data);
     var objData = type === 'each' && !_isArray && typeof _data === 'object' && _data;
 
+    if (helper) { // helpers or inline functions
+      data.helpers[0] = createHelper(helperValue, name.name, keys);
+      if (type === 'if') return helperValue ? fn[0](data) : fn[1] && fn[1](data);
+      else if (type === 'unless') return !helperValue ? fn[0](data) : fn[1] && fn[1](data);
+      else {
+        _data = helperValue;
+        _isArray = isArray(_data);
+      }
+    }
     _data = type === 'unless' ? !_data : objData ? getKeys(_data, []) : _data;
     if (_isArray || objData) {
       if (isNot) return !_data.length ? fn[0](_data) : '';
-      data.path.unshift({}); data.helpers.unshift({});
+      data.path.unshift({}); data.helpers.unshift({}); // faster then getSource()
       for (var n = 0, l = _data.length, out = ''; n < l; n++) {
         data.path[0] = _isArray ? _data[n] : objData[_data[n]];
-        data.helpers[0] = addToHelper({ '@index': '' + n, '@last': n === l - 1, '@first': !n,
-            '.': data.path[0], 'this': data.path[0], '@key': _isArray ? n : _data[n] },
-            keys, _isArray ? n : _data[n], data.path[0]);
+        data.helpers[0] = createHelper(data.path[0], _isArray ? n : _data[n], keys, l, n);
         out = out + fn[0](data);
       }
       data.path.shift(); data.helpers.shift(); // jump back out of scope-level
       return out;
     }
-    var _fn = (!name.strict && _this.options.helpers[name.name]) || (isFunction(_data) && _data);
-    if (_fn) { // helpers or inline functions
-      return _fn.apply(tools(_this, data, vars, fn[0], fn[1]), vars.vars);
-    }
     if (isNot && !_data || !isNot && _data) { // regular replace
-      return fn[0](type === 'unless' || type === 'if' ? data : getSource(data, undefined, _data,
-        addToHelper({ '.': _data, 'this': _data, '@key': name.name }, keys, name.name, _data)));
+      return helper && typeof _data === 'string' ? _data : // comes from helper
+        fn[0](type === 'unless' || type === 'if' ? data : getSource(data, undefined, _data,
+        createHelper(_data, name.name, keys)));
     }
    return fn[1] && fn[1](data); // else
   }
@@ -302,10 +344,13 @@ function sizzleTemplate(_this, html) {
 
   while (_html !== html && (_html = html)) {
     html = html.replace(_this.sectionRegExp, function(all, start, type, name, vars, end, text) {
+      if (type === '#*') {
+        return _this.registerPartial(vars.replace(/(?:^['"]|['"]$)/g, ''), text) && '';
+      }
       text = text.split(_this.elseSplitter);
       sections.push(section(_this, [inline(_this, text[0], sections),
         text[1] && inline(_this, text[1], sections)],
-        name, vars && vars.replace(/\|/g, '').split(/\s+/) || [], start === '{{{', type === '^'));
+        name, vars && vars.replace(/[(|)]/g, '').split(/\s+/) || [], start === '{{{', type === '^'));
       return (tags[0] + '-section- ' + (sections.length - 1) + tags[1]);
     });
   }
