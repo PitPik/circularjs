b33f5f77751326322dc28bd36bd85b2bfe3edae2
peterd <peter@dematte.at>
2018-07-22 11:31:03 +0200

    updated demo; added preload

--- heroes/js/app.js
+++ heroes/js/app.js
@@ -7,27 +7,40 @@ define('app', ['circular'], function(Circular) {
             currentApp: ''
         };
 
-    circular.component('app', {
-        model: [appModel],
-        listeners: ['currentApp'],
-        subscribe: function(property, item, value, oldValue) {
-            value !== oldValue && circular.renderModule({
-                name: value,
-                previousName: oldValue,
-                path: 'modules/' + value + '/index.html',
-                container: item.views['app-modules'],
-                require: 'app-' + value
-            });
-        },
-        onInit: function(component) {
-            appModel.views.title.textContent = appModel.title;
-        }
-    });
-
     circular.addRoute({
         path: '(/:appName)(/*)',
         callback: function(data) {
             appModel.currentApp = data.parameters.appName;
         }
     }, true);
+
+    circular.component('app', {
+        model: [appModel],
+        listeners: ['currentApp'],
+        subscribe: renderModule,
+        onInit: function(component) {
+            appModel.views.title.textContent = appModel.title;
+            appModel.currentApp = appModel.currentApp;
+            preload(['dashboard', 'heroes'], appModel);
+        }
+    });
+
+    function renderModule(property, item, value, oldValue) {
+        circular.renderModule({
+            name: value,
+            previousName: oldValue,
+            path: 'modules/' + value + '/index.html',
+            container: !!property && item.views['app-modules'],
+            require: 'app-' + value,
+            init: !!property,
+            preload: !property
+        });
+    }
+
+    function preload(items, model) {
+        items.forEach(function(item) {
+            model.currentApp !== item &&
+                renderModule(null, model, item, item);
+        });
+    }
 });
