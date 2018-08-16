90dfe1f64d7850e5edc2086a6230fb3237cad365
Peter Dematté <PitPik@users.noreply.github.com>
2018-08-16 11:54:45 +0200

    Update app.js

--- todo/app.js
+++ todo/app.js
@@ -1,9 +1,10 @@
 // Full spec-compliant TodoMVC with localStorage persistence
-// and hash-based routing in ~58 effective lines of JavaScript.
-require(['circular'], Circular => { 'use strict';
+// and hash-based routing in ~75 effective lines of JavaScript.
+require(['circular'], Circular => {
   const ENTER_KEY = 13;
   const ESCAPE_KEY = 27;
   const STORAGE_KEY = 'todos-circularjs-0.1';
+  const get = 'getElementsByProperty';
 
   const circular = new Circular();
   const storage = Circular.Toolbox.storageHelper;
@@ -12,22 +13,26 @@ require(['circular'], Circular => { 'use strict';
     model: storage.fetch(STORAGE_KEY),
     listeners: ['*'],
     subscribe: (property, item) => {
-      property === 'text' ? (item.editable = '') : Circular.Toolbox.lazy(updateUI, list);
-      property !== 'editable' && storage.saveLazy(list.model, STORAGE_KEY);
+      property === 'text' ? item.editable = '' :
+        Circular.Toolbox.lazy(updateUI, list);
+      if (property !== 'editable')
+        storage.saveLazy(list.model, STORAGE_KEY);
     },
     eventListeners: {
       toggle: (e, elm, item) => item.done = elm.checked,
       delete: (e, elm, item) => list.removeChild(item),
       save: (e, elm, item) => item.text = elm.value,
-      edit: (e, elm, item) => item.editable = 'focus',
+      focus: (e, elm, item) => item.editable = 'focus',
       blurItem: (e, elm, item) => item.editable = '',
-      escape: (e, elm, item) => e.keyCode === ESCAPE_KEY && (item.text = item.text),
-    }
+      escape: (e, elm, item) => {
+        if (e.keyCode === ESCAPE_KEY) item.text = item.text;
+      },
+    },
   });
 
   const updateUI = () => {
-    const all = list.getElementsByProperty().length;
-    const done = list.getElementsByProperty('done', true).length;
+    const all = list.model.length;
+    const done = list[get]('done', true).length;
     const appModel = circular.components['app'].model[0];
 
     appModel.all = all !== 0 && all === done;
@@ -38,29 +43,41 @@ require(['circular'], Circular => { 'use strict';
   };
 
   circular.component('app', {
-    model: [{ filter: 'all', left: 0, plural: false, all: false, none: false, hide: true }],
+    model: [{
+      filter: 'all',
+      left: 0,
+      plural: false,
+      all: false,
+      none: false,
+      hide: true,
+    }],
     listeners: ['*'],
     eventListeners: {
       addItem: (e, elm, item) => {
         const text = elm.value.trim();
 
         if (e.keyCode === ENTER_KEY && text) {
-          list.appendChild({ text: text, done: false, editable: '' });
+          list.appendChild({
+            text: text,
+            done: false,
+            editable: '',
+          });
           elm.value = '';
         }
       },
-      deleteDone: (e, elm, item) => list.getElementsByProperty('done', true)
+      deleteDone: (e, elm, item) => list[get]('done', true)
         .forEach(unit => list.removeChild(unit)),
-      toggleAll: (e, elm, item) => list.getElementsByProperty('done', !e.target.checked)
+      toggleAll: (e, elm, item) => list[get]('done', !e.target.checked)
         .forEach(unit => unit.done = e.target.checked),
     },
     onInit: self => {
       circular.addRoute({
         path: '(/)(:filter)',
-        callback: data => self.model[0].filter = data.parameters.filter || 'all',
+        callback: data => self.model[0].filter =
+          data.parameters.filter || 'all',
       }, true);
       updateUI();
       self.model[0].views.main.appendChild(list.container);
-    }
+    },
   });
 });
