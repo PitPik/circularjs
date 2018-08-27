d4b117e66195afa2cd295279ec090be38ab323bc
Peter Dematté <PitPik@users.noreply.github.com>
2018-08-27 10:44:24 +0200

    Update VOM.js

--- VOM.js
+++ VOM.js
@@ -264,11 +264,6 @@ function enhanceModel(_this, model, listeners) {
 
       if (rootOnlyWildcard) {
         for (var item in model) addProperty(_this, item, { current: model });
-      } else if (lastIsWildcard) {
-        for (var item in deepModel) {
-          addProperty(_this, item, { current: deepModel, root: model },
-            path.replace('*', item));
-        }
       } else if (wildcardPos > 0 && listener.length > 1) {
         for (var item in deepModel) {
           if (lastIsWildcard) {
@@ -281,6 +276,11 @@ function enhanceModel(_this, model, listeners) {
             addProperty(_this, deepListener[deepListener.length - 1],
               { current: depperModel, root: model }, path.replace('*', item));
           }
+        }
+      } else if (lastIsWildcard) {
+        for (var item in deepModel) {
+          addProperty(_this, item, { current: deepModel, root: model },
+            path.replace('*', item));
         }
       } else {
         addProperty(_this, listener[listener.length - 1],
