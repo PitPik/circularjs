74c864be10fc4b8c35e2212d2acb05eff83c1d70
peterd <peter@dematte.at>
2018-10-08 09:34:26 +0200

    change date formatting for performance tests

--- conduit/src/js/built/app.all.js
+++ conduit/src/js/built/app.all.js
@@ -9,15 +9,26 @@ define('template-helpers', [], function() {
       return this.getData($1) === this.getData($2) ? this.getBody() : '';
     },
     // ----- inline helpers... ----- //
-    'simple-date': function($1, $2) {
-      const options = {
-        year: 'numeric',
-        month: 'long',
-        day: $2 === 'short' ? undefined : 'numeric',
-      };
+    // 'simple-date': function($1, $2) { // too slow
+    //   const options = {
+    //     year: 'numeric',
+    //     month: 'long',
+    //     day: $2 === 'short' ? undefined : 'numeric',
+    //   };
 
-      return  new Date(this.getData($1))
-        .toLocaleDateString(undefined, options);
+    //   return  new Date(this.getData($1))
+    //     .toLocaleDateString(undefined, options);
+    // },
+    'simple-date': function($1, $2) {
+      const date = new Date(this.getData($1));
+      const monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'];
+
+      const day = date.getDate();
+      const monthIndex = date.getMonth();
+      const year = date.getFullYear();
+
+      return `${monthNames[monthIndex]} ${day}, ${year}`;
     },
     'markdown': function($1) {
       return options.markdown(this.getData($1).trim());
