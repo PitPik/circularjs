b276682ad1a76eacb9daa0f22522d6c6791597a3
peterd <peter@dematte.at>
2018-10-09 14:23:45 +0200

    add article preview feature =)

--- conduit/src/modules/editor/index.js
+++ conduit/src/modules/editor/index.js
@@ -1,7 +1,11 @@
 define('app-editor', ['app-data.srv', 'form-helper'], (dataService, formHelper) =>
 promise =>  promise.then(data => {
   data.cr.component('editor', {
-    model: [data.article ? data.article.article : {}],
+    model: [data.article ? Object.assign(data.article.article, {
+      textLength: data.article.article.body.length > 3,
+      isPreviewing: false,
+    }) : { textLength: 0, isPreviewing: false }],
+    listeners: ['textLength', 'isPreviewing', 'body'],
     eventListeners: {
       submit: (e, elm, item) => {
        const formElements = [].slice.call(item.elements.element.elements); // TODO
@@ -15,6 +19,14 @@ promise =>  promise.then(data => {
         }).catch(error => {
           formHelper.error(error, errorComponent, formElements);
         });
+      },
+      input: (e, elm, item) => {
+        item.body = elm.value;
+        item.textLength = elm.value.length > 3;
+      },
+      preview: (e, elm, item) => {
+        item.views.preview.style.height = item.views.editor.offsetHeight + 'px';
+        item.isPreviewing = !item.isPreviewing;
       },
     },
   });
