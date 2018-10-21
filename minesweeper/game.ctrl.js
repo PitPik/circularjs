3376c0a4bde41f0c6e01849e5a84eb29b2a829d5
peterd <peter@dematte.at>
2018-10-21 12:05:30 +0200

    fix lookAround and loose

--- minesweeper/game.ctrl.js
+++ minesweeper/game.ctrl.js
@@ -23,7 +23,8 @@ define('game-controller', ['game-service'], function(gameSrv) {  'use strict';
   function checkItem(inst, item, mark) {
     if (item.isProcessed && mark !== undefined) {
       lookAround(inst, item, false, 0) === item.surroundingMines &&
-        lookAround(inst, item, true);
+        delete checkItem._win && lookAround(inst, item, true);
+      if (checkItem._win === false) return false;
     } else if (mark && !item.isProcessed) {
       item.mark = item.mark === 'marked' ? 'open' :
         item.mark === 'open' ? '' : 'marked';
@@ -31,13 +32,12 @@ define('game-controller', ['game-service'], function(gameSrv) {  'use strict';
       inst.getElementsByProperty('isMine', true).forEach(function(_item) {
         _item.mark = _item === item ? 'mine last' : 'mine';
       });
-      return false;
+      return checkItem._win = false; // no proper return on lookAround
     } else if (item.mark !== 'marked' && !item.isProcessed) {
       item.mark = '';
       item.isProcessed = true;
       if (!item.surroundingMines) lookAround(inst, item);
     }
-
     return checkAll(inst.getElementsByProperty('isProcessed', false));
   }
 
