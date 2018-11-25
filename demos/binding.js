2fd389c2bfa06a61fda4afe694380f0720d0a5a3
peterd <peter@dematte.at>
2018-11-25 11:59:39 +0100

    update demo "binding"

--- demos/binding.js
+++ demos/binding.js
@@ -1,14 +1,32 @@
-define('app-binding', ['circular'], function(Circular) {
-  new Circular().component('input-test', {
-    model: [{ value: '' }],
-    listeners: ['value'],
-    eventListeners: {
-      input: function(e, elm, item) {
-        item.value = elm.value;
-      },
-      clear: function(e, elm, item) {
-        item.value = '';
-      }
+define('app-binding', ['circular'], Circular => {
+  class Binding {
+    constructor(name, params = {}) {
+      const circular = params.circular || new Circular();
+      const options = Circular.extend({
+        model: [{ value: '' }],
+        listeners: ['value'],
+        eventListeners: { input: this.input, clear: this.clear },
+      }, params);
+
+      this.component = circular.component(name, options);
     }
-  });
+
+    input(e, elm, item) { // can be extended the Circular or ES6 way
+      item.value = elm.value;
+    }
+
+    clear(e, elm, item) {
+      item.value = '';
+    }
+
+    reset(model) {
+      return this.component.reset(model);
+    }
+  }
+  // demos usage as reusable js module
+  define('Binding', [], () => Binding);
+});
+
+require(['Binding'], Binding => {
+  const binding = new Binding('input-test');
 });
