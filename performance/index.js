a642e811e7587f4882db8e796385967ccc0bb68f
Peter Dematté <PitPik@users.noreply.github.com>
2019-10-28 21:13:57 +0100

    update performance demo

--- performance/index.js
+++ performance/index.js
@@ -1,66 +1,78 @@
-require(['circular'], function(Circular) {
-  var circular = new Circular();
+require(['circular'], ({ Component }) => {
 
-  var app = circular.component('app', {
-    listeners: ['items.*'], // 'items.*.value', 'items.*.max'
-  });
+return Component({
+  selector: 'body',
+  template: document.body.innerHTML,
+  $: {
+    model: ['value', 'max'],
+    this: ['levelRaster', 'levelSpeed', 'refresh', 'raster'],
+  }
+}, class Speed {
+  levelRaster = 10;
+  levelSpeed = 400;
+  refresh;
+  raster;
+  model = initModel(10, []);
 
-  var sliders = circular.component('sliders', {
-    model: [{ levelSpeed: 10, levelRaster: 10 }],
-    listeners: ['*'],
-    subscribe: function(propName, item, value) {
-      if (propName === 'levelRaster') {
-        app.model = initModel(value, []);
-        item.views.displayRaster.textContent =
-          value + ' x ' + value + ' = ' + (value * value) + ' x 2 items';
-        item.levelSpeed = item.levelSpeed;
-      } else {
-        item.views.displayRefresh.textContent =
-          value + 'ms (' + (Math.round(1000 / value * 100) / 100) + '/s): ' +
-          (Math.round(item.levelRaster * item.levelRaster *
-          (1000 / value) * 2) + '').replace(/(\d+)(\d{3})$/g, "$1.$2") + ' updates/s';
-      }
-      update(item);
-    },
-    eventListeners: {
-      changeRaster: function(e, elm, item) {
-        item.levelRaster = elm.value;
-      },
-      changeSpeed: function(e, elm, item) {
-        item.levelSpeed = elm.value;
-      },
-    },
-    onInit: function(self) {
-      self.model[0].levelRaster = 10;
-      self.model[0].levelSpeed = 400;
-    }
-  });
-console.log(app.model)
-  function initModel(raster, data) {
-    for (var n = raster, newData = 0; n--; ) {
-      data[n] = { items : [] };
-      for (var m = raster; m--; ) {
-        newData = Math.round(Math.random() * 1000) % 100;
-        data[n].items.push({ value: newData, max: newData > 90 ? 'max' : '' });
-      }
-    }
-    return data;
+  constructor() {
+    document.body.firstElementChild.innerHTML = '';
   }
 
-  function update(model) {
-    clearTimeout(model._timeout);
-    model._timeout = setTimeout(function render() {
-      // window.requestAnimationFrame(function() {
-        for (var n = 0, m = app.model.length, newData = 0; n < m; n++) {
-          for (var x = 0, y = app.model[n].items.length; x < y; x++) {
-            newData = Math.round(Math.random() * 1000) % 100;
-            app.model[n].items[x] = { value: newData, max: newData > 90 ? 'max' : '' };
-            // app.model[n].items[x].foo.value = newData;
-            // app.model[n].items[x].foo.max = newData > 90 ? 'max' : '';
-          }
+  onInit() {
+    this.levelRaster = 10;
+    this.levelSpeed = 400;
+  }
+
+  this$(propName, item, value) {
+    if (propName === 'levelRaster') {
+      this.model = initModel(value, []);
+      this.raster = 
+        value + ' x ' + value + ' = ' + (value * value) + ' x 2 items';
+      update(this.model, this.levelSpeed);
+    } else if (propName === 'levelSpeed') {
+      this.refresh = 
+        value + 'ms (' + (Math.round(1000 / value * 100) / 100) + '/s): ' +
+        (Math.round(this.levelRaster * this.levelRaster *
+        (1000 / value) * 2) + '').replace(/(\d+)(\d{3})$/g, "$1.$2") + ' updates/s';
+      update(this.model, this.levelSpeed);
+    }
+  }
+
+  changeRaster(e, elm, item) {
+    this.levelRaster = elm.value;
+  }
+
+  changeSpeed(e, elm, item) {
+    this.levelSpeed = elm.value;
+  }
+}).init(document.body.firstElementChild);
+
+function initModel(raster, data) {
+  for (var n = raster, newData = 0; n--; ) {
+    data[n] = { childNodes : [] };
+    for (var m = raster; m--; ) {
+      newData = Math.round(Math.random() * 1000) % 100;
+      data[n].childNodes.push({ value: newData, max: newData > 90 ? 'max' : '' });
+    }
+  }
+  return data;
+}
+
+function update(model, levelSpeed) {
+  clearTimeout(model._timeout);
+  model._timeout = setTimeout(function render() {
+    // window.requestAnimationFrame(function() {
+      for (var n = 0, m = model.length, newData = 0; n < m; n++) {
+        for (var x = 0, y = model[n].childNodes.length; x < y; x++) {
+          newData = Math.round(Math.random() * 1000) % 100;
+          
+          model[n].childNodes[x].value = newData;
+          model[n].childNodes[x].max = newData > 90 ? 'max' : '';
         }
-        update(model);
-      // })
-    }, model.levelSpeed);
-  }
+      }
+      update(model, levelSpeed);
+    // })
+  }, levelSpeed);
+}
+
 });
