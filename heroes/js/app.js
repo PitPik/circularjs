ec87f46b87e2fbd45eb5abc0252214b36caf9507
Peter Dematté <PitPik@users.noreply.github.com>
2019-11-08 17:08:48 +0100

    Update app.js

--- heroes/js/app.js
+++ heroes/js/app.js
@@ -1,51 +1,37 @@
-define('app', ['circular'], function(Circular) {
-    'use strict';
+define('app', ['circular'], ({ Module }) => Module({
+  selector: 'app-main',
+  template: `
+    <section class="app-{{%currentApp}}">
+      <h1>{{title}}{{#%currentApp}} - {{.}}{{/%currentApp}}</h1>
+      <nav class="app-nav">
+        <a href="#/dashboard" class="dashboard">Dashboard</a>
+        <a href="#/heroes" class="heroes">Heroes</a>
+      </nav>
+      <div cr-view="app-modules"></div>
+    </section>`,
+  subscribe$: { this: ['currentApp'] },
+}, class AppMain {
+  crInst = {};
+  container = {};
 
-    var circular = new Circular(),
-        appModel = {
-            title: 'Tour of Heroes',
-            currentApp: ''
-        };
+  currentApp = '';
+  title = 'Tour of Heroes';
 
-    circular.addRoute({
-        path: '(/:appName)(/*)',
-        callback: function(data) {
-            appModel.currentApp = data.parameters.appName;
-        }
+  onInit(elm, items, crInst) {
+    this.crInst = crInst;
+    this.container = items.views['app-modules'];
+
+    crInst.addRoute({
+      path: '(/:appName)(/*)',
+      callback: data => this.currentApp = data.parameters.appName,
     }, true);
+  }
 
-    circular.component('app', {
-        model: [appModel],
-        listeners: ['currentApp'],
-        subscribe: renderModule,
-        onInit: function(component) {
-            appModel.currentApp = appModel.currentApp; // init
-            preload(['dashboard', 'heroes'], appModel);
-        }
+  this$(property, item, value) {
+    value && this.crInst.renderModule({
+      selector: 'app-' + value,
+      container: this.container,
+      init: true,
     });
-
-    function renderModule(property, item, value, oldValue) {
-        circular.renderModule({
-            name: value,
-            previousName: oldValue,
-            path: 'modules/' + value + '/index.html',
-            container: !!property && item.views['app-modules'],
-            require: 'app-' + value,
-            init: !!property,
-            preload: !property,
-            transition: function(data) {
-                data.promise.then(function() {
-                    data.remove();
-                    data.append();
-                });
-            }
-        });
-    }
-
-    function preload(items, model) {
-        items.forEach(function(item) {
-            model.currentApp !== item &&
-                renderModule(null, model, item, item);
-        });
-    }
-});
+  }
+}));
