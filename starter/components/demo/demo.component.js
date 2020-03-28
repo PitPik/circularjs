c40ddd35d4a04729d27f56820d6848f7c97644ee
Peter Dematté <PitPik@users.noreply.github.com>
2020-03-28 11:35:48 +0100

    update starter demo

--- starter/components/demo/demo.component.js
+++ starter/components/demo/demo.component.js
@@ -5,11 +5,12 @@ Component({
   selector: 'demo',
   template: `
     <h2 class="{{%class}}" cr-event="click: clickMe">
-      test is also working!!
+      test is also working!! <small>(click to change color)<small>
     </h2>`,
   styles: `
     .test-class-foo { color: red }
-    .test-class-foo-foo { color: blue }`,
+    .test-class-foo-foo { color: green }
+    .test-class-foo-foo-foo { color: blue }`,
   subscribe$: { this: ['class'] },
 }, class Demo {
   constructor(elm, crInst, input) {
